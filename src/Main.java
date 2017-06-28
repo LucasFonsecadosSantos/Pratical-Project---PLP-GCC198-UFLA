b1e0fae0cc22f9ff1e1a391d8620adf27b069e7f
Lucas Fonseca <lucas@lcfcompany.com.br>
2017-06-28 12:43:43 +0000

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ *
+ *  CITY MANAGER 1.0 June 26, 2017.
+ *  Pratical Project by Paradigms and Programming Languages
+ *  GCC198 - Federal University of Lavras (UFLA)
+ *
+ *  @author Lucas Fonseca dos Santos
+ *  @author Leticia Ferreira
+ */
+package main;
+
+import app.System;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System system = new System();
+        system.execute();
+    }
+    
+}
---
+++ src/util/Data.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *  CITY MANAGER 1.0 June 26, 2017.
+ *  Pratical Project by Paradigms and Programming Languages
+ *  GCC198 - Federal University of Lavras (UFLA)
+ *
+ *  @author Lucas Fonseca dos Santos
+ *  @author Leticia Ferreira
+ */
+package util;
+
+public class Data {
+    
+}
